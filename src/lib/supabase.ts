bdf413e2f442638e3556e61a908ef8b4fe76e236
Charle56378492hj <hsn941291@gmail.com>
2026-08-14 22:55:03 -0500

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -9,8 +9,8 @@ if (!supabaseUrl || !supabaseAnonKey) {
 }
 
 export const supabase = createClient(
-  supabaseUrl ?? 'https://uxoqmeovbtnjzbfjwajx.supabase.co',
-  supabaseAnonKey ?? 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV4b3FtZW92YnRuanpiZmp3YWp4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUwNzI1MTAsImV4cCI6MjEwMDY0ODUxMH0.3nQ74Y9r7SxdvtfWp9zXjRtkbAdODtNqPBGw1E77JZ4',
+  supabaseUrl ?? 'https://rxwfjhzszapygiouohga.supabase.co',
+  supabaseAnonKey ?? 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ4d2ZqaHpzemFweWdpb3VvaGdhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY2NzczODMsImV4cCI6MjEwMjI1MzM4M30.HBpofY22gX8ynsCJu-x2aHE7IucPRdtL9WRQWyOYYdM',
   {
     auth: {
       persistSession: true,
